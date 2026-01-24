c597acc2886fda2b0fa9717361419d5730ddd55f
neuroproxy <cgcj979@gmail.com>
2026-01-24 15:38:14 -0500

    mitd de proyeto 06: FINAl proyecto

--- 06-shopping-car/src/context/cart.jsx
+++ 06-shopping-car/src/context/cart.jsx
@@ -1,40 +1,36 @@
-import { createContext, useState } from "react"; 
+import { useReducer } from "react";
+import { createContext } from "react"; 
+import {cartReducer, cartInitialState} from "../reducers/cart"
 
 export const CartContext = createContext()
 
+function useCartReducer() {
+    const [state, dispatch] = useReducer(cartReducer, cartInitialState)
+
+    const addToCart = product => dispatch({
+        type: 'ADD_TO_CART',
+        payload: product
+    })
+
+    const removeFromCart = product => dispatch({
+        type: 'REMOVE_FROM_CART',
+        payload: product
+    })
+
+    const cleanCart = () => dispatch({
+        type: 'CLEAR_CART'
+    })
+
+    return { state, addToCart, removeFromCart, cleanCart }
+}
+
 export function CartProvider ({ children }) {
-    const [cart, setCart] = useState([])
 
-    const addToCart = product => {
-        const productInCartIndex = cart.findIndex(item => item.id === product.id)
-
-        if (productInCartIndex >= 0) {
-            const newCart = structuredClone(cart)
-            newCart[productInCartIndex].quantify +=1
-            return setCart(newCart)
-        }
-
-        setCart(prevState => ([
-            ...(prevState),
-            {
-                ...product,
-                quantify: 1
-            }
-        ]))
-
-    }
-
-    const removeFromCart = product => {
-        setCart(prevState => prevState.filter(item => item.id !== product.id))
-    }
-
-    const cleanCart = () => {
-        setCart([])
-    }
+    const { state, addToCart, removeFromCart, cleanCart } = useCartReducer()
 
     return(
         <CartContext.Provider value={{
-            cart,
+            cart: state,
             addToCart,
             removeFromCart,
             cleanCart
